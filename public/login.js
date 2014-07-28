55c0caa300130f6b8334e23e4412b16546c1615b
kuiw <kui.wfly@gmail.com>
2014-07-28 21:23:16 +0800

    change dialog id and dialog can show normally old id is diagLogin it is not work, but now it can work with diag-login

--- public/login.js
+++ public/login.js
@@ -1,14 +1,15 @@
 /**
  * New node file
  */
-var diagLogin,formLogin;
-function Login(){
-	alert("login");
-	diagLogin.dialog("close");
-}
+
 $(function(){
-	diagLogin = $("#diagLogin").dialog({
-		autoOpen:false,
+	var diagLogin,formLogin;
+	function Login(){
+		alert("login");
+		diagLogin.dialog("close");
+	}
+	diagLogin = $("#diag-login").dialog({
+		autoOpen:true,
 		height:300,
 		width:350,
 		modal:true,
@@ -26,7 +27,16 @@ $(function(){
 	formLogin = diagLogin.find("form").on("submit",function(){
 		alert("submit");
 	});
-	
+
+    /*
+    $( "#dialog-message" ).dialog({
+        modal: true,
+        buttons: {
+          Ok: function() {
+            $( this ).dialog( "close" );
+          }
+        }
+      });*/
 	}
 );
 $(document).ready(function(){
